c34e41e5dd2f5a82267f9e89babd16659d62a5b5
Mikko <msoft@hotmail.fi>
2018-11-26 19:02:16 +0200

    Added first version of the product list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { ProductList } from '../../providers/productlist/productlist';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  product: string = "..";
+
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, public productList: ProductList) {
 
   }
 
+  readProduct() {
+  	console.log('>> home.readProduct');
+  	this.init();
+
+  	/*this.barcodeScanner.scan().then(barcodeData => {
+ 		console.log('Barcode data', barcodeData);
+ 		this.product = barcodeData.text;
+	}).catch(err => {
+    	console.log('Error', err);
+	});*/
+  }
+
+  init() {
+    console.log('>> home.init');
+   	console.log(this.productList.getProductList());
+    //console.log(this.productList.getTestObj());
+  }
 }
