98b58397f2d9122c0ad5145264254512fe2452f7
Mikko <msoft@hotmail.fi>
2018-11-27 22:27:52 +0200

    Added product search

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,27 +9,45 @@ import { ProductList } from '../../providers/productlist/productlist';
 })
 export class HomePage {
 
-  product: string = "..";
+  productNumber: string = "..";
+  productName: string = "..";
+  productNameInitials: string = "";
+
+  //productInfo = {ISBN:'',Tuotenro:'',Tuote:'',Kpl:'',Hinta:''};
 
   constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, public productList: ProductList) {
-
-  }
-
-  readProduct() {
-  	console.log('>> home.readProduct');
-  	this.init();
-
-  	/*this.barcodeScanner.scan().then(barcodeData => {
- 		console.log('Barcode data', barcodeData);
- 		this.product = barcodeData.text;
-	}).catch(err => {
-    	console.log('Error', err);
-	});*/
   }
 
   init() {
     console.log('>> home.init');
-   	console.log(this.productList.getProductList());
+    console.log(this.productList.getProductList());
     //console.log(this.productList.getTestObj());
+  }
+
+  readProduct() {
+    console.log('>> home.readProduct');
+    this.init();
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.productNumber = barcodeData.text;
+      var productInfo = this.productList.getProduct(this.productNumber);
+      this.productName = productInfo.Tuote;
+      console.log('>> ' + JSON.stringify(productInfo));
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+
+  onProductNameUpdated() {
+    console.log('>> home.onProductNameUpdated: ' + this.productNameInitials);
+    var found = this.productList.getProductName(this.productNameInitials);
+    console.log('>> found: ' + found.length);
+    console.log('>> json: ' + JSON.stringify(found));
+    if (found.length == 1) {
+      console.log('>> found item: ' + found[0].Tuote);
+      this.productName = found[0].Tuote;
+      this.productNumber = found[0].ISBN;
+    }
   }
 }
